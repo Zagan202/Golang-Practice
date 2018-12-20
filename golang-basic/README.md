7669a70a46c0afe4db24fb8fcc70bb14243aae15
Lawrence Lawson <lawrenceterralawson@gmail.com>
2018-12-19 19:46:26 -0800

    simple stuff

---
+++ golang-basic/README.md
@@ -0,0 +1,26 @@
+# Golang Basic
+This section will cover the simple fundamentals of Golang
+
+## The fundamentals demostrated are the following
+
+1. Data Types
+2. Integer, float, strinfg
+3. Booleans
+4. Variables
+5. Constants
+6. Operators
+7. Functions
+8. Deferred Functions
+9. Calls
+10. Variadic Functions
+11. Panic and Recover
+12. Arrays
+13. Slices
+14. Maps
+15. Structs
+16. Interfaces
+17. Concurrency
+18. Channels
+19. Logging
+20. Files and Directories
+21. Cryptography
---
+++ golang-basic/data-type.go
@@ -0,0 +1,56 @@
+/*
+This is simply going over the way with variables
+as a statically typed language
+*/
+
+package main
+
+//https://talks.golang.org/2014/organizeio.slide#3
+/*
+All Go source is part of a package.
+Every file begins with a package statement.
+Programs start in package main.
+
+For very small programs, main is the only package you need to write.
+*/
+
+import "fmt"
+
+//https://golang.org/pkg/fmt/
+//fmt package implements I/O with functions analogous to C's printf and scanf.
+
+var (
+	a int
+	b int
+)
+
+//Globally declaring two ints "a" & "b"
+
+func main() {
+	var x int = 1
+	var y int
+	//integer data types
+	fmt.Println(x) //Should print 1
+	fmt.Println(y) //Should print 0
+
+	var t, e, s, ts = 1.2, 2.3, 3.4, 4.5
+	//Declaring multiple float32 vars
+	fmt.Println(t, e, s, ts)
+
+	location := "Los Angeles" //String declaration
+	/*
+		https://tour.golang.org/basics/10
+		Short variable declarations
+		:= short assignment statement can be used in
+		place of a var declaration with implicit type.
+	*/
+	Country := "USA! USA! USA!"
+	fmt.Println(location)
+	fmt.Println(Country)
+
+	alice, bob, jeff := "String", 20, 3.4 //Multiple type declarations using short assignment
+	fmt.Println(alice, bob, jeff)
+
+	a, b = 12, 43
+	fmt.Println(a, b)
+}
